f637b61574d93ad03e99b1bbad2865fc4978ba72
Kipiru <dtaranets@gmail.com>
2021-11-04 19:51:25 +0300

    added try...catch for NoResultException

--- src/main/java/hiber/MainApp.java
+++ src/main/java/hiber/MainApp.java
@@ -6,11 +6,12 @@ import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import javax.persistence.NoResultException;
 import java.sql.SQLException;
 import java.util.List;
 
 public class MainApp {
-   public static void main(String[] args) throws SQLException {
+   public static void main(String[] args) {
       AnnotationConfigApplicationContext context = 
             new AnnotationConfigApplicationContext(AppConfig.class);
 
@@ -36,7 +37,11 @@ public class MainApp {
       }
 
       System.out.println("Get User by Car:");
-      System.out.println(userService.getUserByCar("Porsche", 911));
+      try {
+         System.out.println(userService.getUserByCar("Porsche", 911));
+      }catch (NoResultException e) {
+         System.out.println("No result");
+      }
 
       context.close();
    }
